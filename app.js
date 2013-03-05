aec18a55ad8f201156f3d9d3b62ada342b853171
zhaoyou <zhaoyou.xt@gmail.com>
2013-03-06 00:26:55 +0800

    basic chat finish

--- app.js
+++ app.js
@@ -29,15 +29,63 @@ io.sockets.on('connection', function (socket) {
       'userId': data.userId,
       'clientId': socket.id
     });
+
+    // send for other online userlist.
+    io.sockets.emit("onlineuser", onlinelist);
+
+    // send to self online userlist.
+  //  socket.emit("onlineuser", onlinelist);
   });
 
   socket.on('message', function (data) {
-    messages.push(data);
+    console.log('data.to',  data.to, data.msg);
+    var message = {
+      'sender': getUserId(socket.id),
+      'msg': data.msg
+    };
+    if (data.to == 'all') {
+      messages.push(message);
+      socket.broadcast.emit('message', message);
+      console.log('from user message', message);
+    } else {
+      io.sockets.socket[getClientId(data.to)].emit('message', message);
+    }
     console.log('receive: ' + data);
-    socket.broadcast.emit('message', data);
+  });
+
+
+  socket.on('disconnect', function() {
+    var userId = '';
+    for (var i = 0; i < onlinelist.length; i++) {
+      var client = onlinelist[i];
+      if (client.clientId == socket.id) {
+        userId = client.userId;
+        onlinelist.splice(i, 1);
+      }
+    }
+    console.log("user disconnect", userId);
+    socket.broadcast.emit("onlineuser", onlinelist);
   });
 
   messages.forEach(function(msg){
-    socket.send(msg);
+    socket.emit('message', msg);
   });
+
 });
+
+function getClientId(userId) {
+  for (var i = 0; i < onlinelist.length; i++) {
+    if (onlinelist[i].userId == userId) {
+      return onlinelist[i].clientId;
+    }
+  }
+}
+
+function getUserId(clientId) {
+  for (var j = 0; j < onlinelist.length; j++) {
+    if (onlinelist[j].clientId == clientId) {
+      console.log(clientId, onlinelist[j].userId);
+      return onlinelist[j].userId;
+    }
+  }
+}
